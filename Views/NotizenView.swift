741c590111b4dcef6bfa5602aae37b1a02ad90da
mr-luc <u.lucas@schulzentrumoe.de>
2026-03-31 19:02:19 +0200

    NotizenView hinzugefügt

---
+++ Views/NotizenView.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+struct NotizenView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Notizen kommen hier hin")
+                .navigationTitle("Notizen")
+        }
+    }
+}
